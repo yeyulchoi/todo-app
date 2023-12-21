d2292924fe21e903346f09f62930c0ac97d733d4
yeyulchoi <yeyulchoi@outlook.com>
2023-12-20 23:59:08 -0500

    Implement the Edit Button Dec 20,2023

--- app01/gui.py
+++ app01/gui.py
@@ -7,8 +7,13 @@ label = sg.Text("Type in a To-Do")
 input_box= sg.InputText(tooltip="Enter ToDo", key='todo')
 add_button = sg.Button("Add")
 
+list_box = sg.Listbox(functions.get_todos(), key="todos",
+                      enable_events=True, size=[45,12])
+edit_button = sg.Button("Edit")
+
 window = sg.Window('My To-Do App',
-                   layout=[[label],[input_box,add_button]],
+                   layout=[[label],[input_box,add_button],
+                            [list_box,edit_button]],
                    font =('Helvetica',15))
 while True:
     event, windowValues = window.read()
@@ -20,6 +25,17 @@ while True:
             new_todos=windowValues["todo"] + "\n"
             todos.append(new_todos)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = windowValues['todos'][0]
+            substitute=windowValues['todo']
+            todos = functions.get_todos()
+            myIndex=todos.index(todo_to_edit)
+            todos[myIndex]=substitute +"\n"
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=windowValues['todos'][0])
         case sg.WIN_CLOSED:
             break
 
