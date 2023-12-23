47972dd4819ee4b3216afbb33b79e5d521c789dd
yeyulchoi <yeyulchoi@outlook.com>
2023-12-23 00:29:58 -0500

    Add Delete feature Dec 23,2023

--- app01/web.py
+++ app01/web.py
@@ -8,17 +8,24 @@ def get_new_todo():
     todo = st.session_state["new_todo"]+'\n'
     todos.append(todo)
     functions.write_todos(todos)
-
+    st.session_state.new_todo = ""
 
 st.title("Python TO-DO List")
-st.subheader(" This is my todo list app")
+st.subheader(" This is Yeyul's todo list app")
 st.write("This app is to increase your productivity")
-
-for index,todo in enumerate(todos):
-    st.checkbox(todo, key=f"{index}_todo")
 
 st.text_input(label="", placeholder=" Add your todo...",
                        on_change=get_new_todo,key='new_todo')
+
+for index,todo in enumerate(todos):
+    checkbox=st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
 
 
 
